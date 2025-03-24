5231fbf43d4ca8fa871f6ba0b8674d59e01b73aa
Charles7458 <c.charles8547@gmail.com>
2025-03-24 22:14:51 +0530

    Added NoteApp.js, NoteApp.css and README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# Overview:
+    This is a personal project of mine, in my journey of learning React.
+    
+# Features:
+    Responsive, uses localStorage so retains the notes you save.
---
+++ src/NoteApp.jsx
@@ -0,0 +1,257 @@
+import { useState, useReducer, useMemo, useEffect } from 'react';
+import './styles/NoteApp.css'
+// features:
+//1. Add notes. 2. edit note. 3. delete a note. 4.search based on title of note
+
+function noteReducer(notes, action) {
+    switch (action.type) {
+        case 'add': 
+        {
+            console.log(`note added. ID: ${action.id}`);
+           return [...notes, {
+            id: action.id,
+            title: action.title,
+            content: action.content,
+           }];
+        }
+
+        case 'edit': {
+            return notes.map( (note) => {
+                if (note.id === action.id) {
+                    return note = {
+                        id: note.id,
+                        title: action.title,
+                        content: action.content
+                    }
+                }
+                else {
+                    return note;
+                }
+            })
+        }
+
+        case 'delete': {
+            return notes.filter((note)=> note.id !== action.id)
+        }
+
+        default: {
+            alert("unidentified action");
+            return null;
+        }
+    }
+}
+
+function DelPopup({isOpen, onYes, onNo}) {
+    if(!isOpen){
+        return null;
+    }
+    return (
+        <div className='popup-div'>
+            <div className='smol-popup'>
+                <h3>Are you sure you want to delete this note?</h3>
+                <div style={{display:"flex", width:"100%", justifyContent:"space-evenly", margin:"30px 0px 20px 0px"}}>
+                    <button style={{backgroundColor:"lightcoral", color:"red", padding:"10px 30px", borderRadius:"10px", cursor:"pointer"}} onClick={onYes}> YES</button>
+                    <button style={{backgroundColor:"lightgreen", color:"green", padding:"10px 30px", borderRadius:"10px", cursor:"pointer"}} onClick={onNo} > NO</button>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+function PopUp({isOpen, id=null, title, content, onSave, onEdit, onClose, action}) {
+    let [newTitle, setNewTitle] = useState(title);
+    let [newContent, setNewContent] = useState(content);
+
+    if (!isOpen){
+        return null;
+    }
+
+    function handleSave() {
+        onSave({title: newTitle, content: newContent});
+        setNewTitle();
+        setNewContent();
+    }
+
+    function handleEdit() {
+        onEdit({id: id, title: newTitle, content: newContent});
+        setNewTitle();
+        setNewContent();
+    }
+
+    function handleClose() {
+        onClose()
+    }
+
+    return(
+        <div className='popup-div'>
+            <div className='popup'>
+                <div style={{display:"flex", margin:"5px 0px", justifyContent:"flex-end"}}>
+                    <span onClick={e=> {e.stopPropagation; handleClose()}} style={{cursor:"pointer"}}><box-icon name="x" size="m" color="#ffffff"></box-icon></span>
+                </div>
+                <b>Title:</b> <br/>
+                <input type='text' value={newTitle} onChange={e => setNewTitle(e.target.value)} className='title-input' /> <br />
+                <b>Content:</b> <br />
+                <textarea  value={newContent} onChange={e => setNewContent(e.target.value)} className='content-input'></textarea>
+                <button className='save-note btn' onClick={action=="edit" ? handleEdit : handleSave}> {action=="edit" ? "Edit Note": "Save Note"}</button>
+            </div>
+        </div>
+    )
+}
+
+function Note({id, title, content, onEdit, onDelete}) {
+    function editClick() {
+        onEdit(id,title, content)
+    }
+
+    function deleteClick() {
+        onDelete(id)
+    }
+
+    return (
+        <div className='note'>
+
+            <div className='note-head'>
+                <span onClick={editClick} style={{cursor:"pointer"}}><box-icon type='solid' name='edit' color='#ffffff'></box-icon></span>
+                <span onClick={deleteClick} style={{cursor:"pointer"}}><box-icon type='solid' name='x-square' color='#ffffff'></box-icon></span>
+            </div>
+
+            <div className='note-text'>
+                <h2 className='note-title' style={{textDecoration:"underline", marginBottom:"15px"}}>{title}</h2> 
+                <p className='note-para'>{content}</p>
+            </div>
+        </div>
+    )
+}
+
+function NoteApp() {
+
+    const [nextId, setNextId] = useState(1);
+    const initialNotes=[
+        {
+            id: 0,
+            title:"Intro",
+            content: "Hello User, this is a new note taking app. Press the + icon to add a new note. You can also edit and delete a note by clicking the ... menu"
+        }
+    ]
+
+    const [notes, dispatch] = useReducer(noteReducer, JSON.parse(localStorage.getItem("notes")) || initialNotes);
+    const [showPopup, setShowPopup] = useState(false);
+    const [showDelPopup, setShowDelPopup] = useState(false);
+    const [selectedNote, setSelectedNote] = useState();
+    const popupTitle = useMemo( ()=> (selectedNote!== undefined ? notes.find( (note)=> note.id === selectedNote).title : undefined), [selectedNote]) ;
+    const popupContent = useMemo( ()=> (selectedNote!== undefined ? notes.find( (note)=> note.id === selectedNote).content : undefined) ,[selectedNote]) ;
+    const [search, setSearch] = useState("");
+    const [searchText, setSearchText] = useState("");
+    const [date, setDate] = useState(new Date());
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    const day = days[date.getDay()];
+
+    useEffect( ()=> {
+        localStorage.setItem("notes", JSON.stringify(notes))
+    }, [notes]);
+
+    function handleAddClick() {
+        setShowPopup(true);
+    }
+
+    function handlEditClick(id) {
+        setSelectedNote(id);
+        setShowPopup(true);
+    }
+
+    function handleDeleteClick(id) {
+        setSelectedNote(id)
+        setShowDelPopup(true);
+    }
+
+    function handleAddNotes(note) {
+        const newNote = {
+            id: nextId,
+            title: note.title,
+            content: note.content
+        }
+        setNextId(nextId+1);
+        setShowPopup(false);
+
+        dispatch(
+            {
+                type: "add",
+                id: newNote.id,
+                title: newNote.title,
+                content: newNote.content,
+            })
+    }
+
+    function handleEditNotes(note) {
+        setShowPopup(false);
+        dispatch(
+            {
+                type: "edit",
+                id: note.id,
+                title: note.title,
+                content: note.content
+            }
+        )
+        setSelectedNote();
+    }
+
+    function handleDeleteNotes(){
+        setShowDelPopup(false);
+        dispatch(
+            {
+                type: "delete",
+                id: selectedNote
+            }
+        )
+        setSelectedNote();
+    }
+
+    function handleSearch() {
+        setSearch(searchText);
+    }
+    
+    return ( 
+
+        <div className='NoteApp'>
+            <h1 className='heading'>Note App</h1>
+            <h3 style={{color:"gray"}}>{date.toLocaleDateString()}, {day}</h3>
+            <input type='text' placeholder='Search a note title' className='search' value={search} onChange={e => setSearch(e.target.value)}/>
+            <span className='clear-btn' onClick={()=>setSearch("")}><box-icon name='x' size='sm'></box-icon></span>
+            <DelPopup isOpen={showDelPopup} onYes={handleDeleteNotes} onNo={()=>setShowDelPopup(false)}/>
+            <PopUp key={selectedNote} isOpen={showPopup} id={selectedNote} title={popupTitle} content={popupContent} action={selectedNote===undefined ? "add": 'edit'} onSave={handleAddNotes} onEdit={handleEditNotes} onClose={()=>{setShowPopup(false); setSelectedNote()}}/>
+
+            <button className='add-note-btn' onClick={handleAddClick}>
+                +
+            </button>
+
+            { (notes.length > 0) && 
+
+                <div className='notes'>
+                    
+                    {
+                        search.length > 0 ? 
+                        
+                        notes.map( (note) =>
+                        {   
+                            let title = `${note.title}`.toLowerCase();
+                            let searchText = search.toLowerCase();
+                            if(title.includes(searchText)){
+                                return <Note key={note.id} id={note.id} title={note.title} content={note.content} onEdit={handlEditClick} onDelete={handleDeleteClick} />
+                            }
+                        }
+                        )
+                        :
+                        notes.map( (note) =>
+                            <Note key={note.id} id={note.id} title={note.title} content={note.content} onEdit={handlEditClick} onDelete={handleDeleteClick} />
+                        )
+                    }
+                </div>
+
+            }
+
+        </div>
+     );
+}
+
+export default NoteApp;
